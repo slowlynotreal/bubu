1e70cdce3de75b561699fc10adc44bc5c78853df
slowlynotreal <tamizhinianhps@gmail.com>
2026-07-31 23:52:56 +0530

    Update cause.js

--- cause.js
+++ cause.js
@@ -76,7 +76,7 @@ function displayNewReason() {
                 duration: 0.5,
                 ease: "elastic.out",
                 onComplete: () => {
-                    shuffleButton.textContent = "Enter Our Storylane 💫";
+                    shuffleButton.textContent = "Entre dans nos souvenirs, mon amour.❤️";
                     shuffleButton.classList.add('story-mode');
                     shuffleButton.addEventListener('click', () => {
                         gsap.to('body', {
